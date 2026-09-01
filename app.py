c70f69a17727b52207a4ac12964c192ff75b5472
Acheron <55134961+DamienEvrard@users.noreply.github.com>
2026-09-01 22:34:46 +0200

    maj port pour render
    Moved port initialization to a separate line for clarity.

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ if __name__ == "__main__":
         host="0.0.0.0" if os.environ.get("PORT") else "127.0.0.1",
         port=int(os.environ.get("PORT", 5000))
     )
+port = int(os.environ.get("PORT", 5000))
 
 # Chargee une seule fois au demarrage du serveur
 db = Database()
